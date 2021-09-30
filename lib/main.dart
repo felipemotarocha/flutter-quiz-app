ab31f4a4ef59d3f704b355af7cfc408d869eb1fa
Felipe Rocha <felipemota.rocha@gmail.com>
2021-09-30 19:44:40 -0300

    feat: add question widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,56 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+import 'question.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex += 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First App'),
         ),
-        body: const Text('This is my first app!'),
+        body: Column(
+          children: [
+            Question(questions[questionIndex]),
+            RaisedButton(
+              child: const Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: const Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: const Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/question.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  const Question(this.questionText, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(questionText);
+  }
+}
